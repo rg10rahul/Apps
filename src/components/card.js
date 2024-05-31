565919ba94d20fc8d9fe0aa8f7f87a5d396d1bd4
rg10rahul <rgrahul149@gmail.com>
2024-05-31 18:43:00 +0530

    Update card.js

--- src/components/card.js
+++ src/components/card.js
@@ -1,1 +1,15 @@
+import React from 'react';
+import './card.css';
 
+const Card = ({ imgSrc, title, description }) => (
+    <div className="cards">
+        <img src={imgSrc} alt={title} />
+        <div className="content">
+            <h3>{title}</h3>
+            <p>{description}</p>
+            <a href="#" className="btn">Read More</a>
+        </div>
+    </div>
+);
+
+export default Card;
